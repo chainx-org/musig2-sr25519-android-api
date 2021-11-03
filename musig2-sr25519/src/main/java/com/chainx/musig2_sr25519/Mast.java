835a290cfbafa071bbb84fb477bafdb7fa16d66b
hacpy <hacpy.lbx@gmail.com>
2021-11-03 15:42:43 +0800

    Use jni format library

--- musig2-sr25519/src/main/java/com/chainx/musig2_sr25519/Mast.java
+++ musig2-sr25519/src/main/java/com/chainx/musig2_sr25519/Mast.java
@@ -2,18 +2,20 @@ package com.chainx.musig2_sr25519;
 
 import android.text.TextUtils;
 
-import com.sun.jna.Native;
-
 public class Mast {
+    static {
+        System.loadLibrary("musig2_sr25519_dll");
+    }
+
+    public static native String generate_threshold_pubkey(String jarg1, long jarg2);
+
+    public static native String generate_control_block(String jarg1, long jarg2, String jarg3);
+    
     public static String generateThresholdPubkey(String[] pubkeys, byte threshold) {
-        return clib.generate_threshold_pubkey(TextUtils.join("", pubkeys).toString(), threshold);
+        return generate_threshold_pubkey(TextUtils.join("", pubkeys).toString(), threshold);
     }
 
     public static String generateControlBlock(String[] pubkeys, byte threshold, String sigAggPubkey) {
-        return clib.generate_control_block(TextUtils.join("", pubkeys).toString(), threshold, sigAggPubkey);
+        return generate_control_block(TextUtils.join("", pubkeys).toString(), threshold, sigAggPubkey);
     }
-
-    final static CLibrary clib = (CLibrary) Native.load(
-            "musig2_sr25519_dll",
-            CLibrary.class);
 }
